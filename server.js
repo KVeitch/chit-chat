95c6518c775433e3d90486010f05b3635239e6c8
KVeitch <kirk.a.veitch@gmail.com>
2020-08-13 22:57:31 -0600

    Create multiple streams for multiple users

--- server.js
+++ server.js
@@ -3,10 +3,15 @@ const app = express();
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const { v4: uuidV4 } = require('uuid')
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+  debug: true
+});
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 
+app.use('/peerjs', peerServer);
 app.get('/', (req, res) => {
   res.redirect(`/${uuidV4()}`)
 })
@@ -16,11 +21,10 @@ app.get('/:room', (req, res) => {
 })
 
 io.on('connection', socket => {
-  socket.on('join-room', () => {
-    console.log('join-room');
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit('user-connected', userId)
   }); 
-
-
 })
 
 
