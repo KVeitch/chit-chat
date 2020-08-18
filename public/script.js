a8492a55750c7ea234845924daef45574767a145
KVeitch <kirk.a.veitch@gmail.com>
2020-08-17 22:56:38 -0600

    Add functionality to toggle own video stream

--- public/script.js
+++ public/script.js
@@ -82,11 +82,9 @@ const toggleMute = () => {
   const enabled = myVideoStream.getAudioTracks()[0].enabled;
   if (enabled) {
     myVideoStream.getAudioTracks()[0].enabled = false;
-    // setUnmuteButton();
     toggleMuteBtn(enabled)
   } else {
     toggleMuteBtn(enabled)
-    // setMuteButton();
     myVideoStream.getAudioTracks()[0].enabled = true;
   }
 }
@@ -98,18 +96,38 @@ const toggleMuteBtn = (isMuted) => {
   document.querySelector('.main__mute_button').innerHTML = html;
 }
 
-// const setMuteButton = () => {
-//   const html = `
-//     <i class="fas fa-microphone"></i>
-//     <span>Mute</span>
-//   `
-//   document.querySelector('.main__mute_button').innerHTML = html;
-// }
+const playStop = () => {
+  console.log('object')
+  let enabled = myVideoStream.getVideoTracks()[0].enabled;
+  if (enabled) {
+    myVideoStream.getVideoTracks()[0].enabled = false;
+    togglePlayVideoBtn(enabled)
+    // setPlayVideo()
+  } else {
+    // setStopVideo()
+    togglePlayVideoBtn(enabled)
+    myVideoStream.getVideoTracks()[0].enabled = true;
+  }
+}
 
-// const setUnmuteButton = () => {
-//   const html = `
-//     <i class="unmute fas fa-microphone-slash"></i>
-//     <span>Unmute</span>
-//   `
-//   document.querySelector('.main__mute_button').innerHTML = html;
-// }
+const togglePlayVideoBtn = (isEnabled) => {
+  let html = isEnabled?`<i class="stop fas fa-video-slash"></i><span>Play Video</span>`:
+  `<i class="fas fa-video"></i><span>Stop Video</span>`;
+  document.querySelector('.main__video_button').innerHTML = html
+}
+
+const setStopVideo = () => {
+  const html = `
+    <i class="fas fa-video"></i>
+    <span>Stop Video</span>
+  `
+  document.querySelector('.main__video_button').innerHTML = html;
+}
+
+const setPlayVideo = () => {
+  const html = `
+  <i class="stop fas fa-video-slash"></i>
+    <span>Play Video</span>
+  `
+  document.querySelector('.main__video_button').innerHTML = html;
+}
